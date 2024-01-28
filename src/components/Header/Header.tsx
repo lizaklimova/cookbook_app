c6fa967572723c47b2801bed4795bf44434bb7ba
lizaklimova <132839549+lizaklimova@users.noreply.github.com>
2024-01-28 21:09:59 +0200

    added navigation bar

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,5 @@
 import { FC } from 'react';
+import { NavLink } from 'react-router-dom';
 import { MainContainer } from 'components/App/App.styled';
 import ToggleBtn from './ToggleBtn';
 
@@ -6,6 +7,25 @@ const Header: FC = () => {
   return (
     <header>
       <MainContainer>
+        <header>
+          <nav
+            style={{
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              gap: '30px',
+              marginBottom: '50px',
+            }}
+          >
+            <NavLink to="/main">Main</NavLink>
+            <NavLink to="/categories/beef">Categories</NavLink>
+            <NavLink to="add">Add recipes</NavLink>
+            <NavLink to="my">My recipes</NavLink>
+            <NavLink to="favorite">Favorites</NavLink>
+            <NavLink to="/shopping-list">Shopping list</NavLink>
+            <NavLink to="search">Search</NavLink>
+          </nav>
+        </header>
         <ToggleBtn />
       </MainContainer>
     </header>
