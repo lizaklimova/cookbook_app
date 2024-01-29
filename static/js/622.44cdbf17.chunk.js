28b62fd4e44ea390aa1c4757700f7d8bf9cff780
lizaklimova <132839549+lizaklimova@users.noreply.github.com>
2024-01-29 12:52:27 +0000

    Deploying to gh-pages from @ lizaklimova/cookbook_app@901b2bf01258f40b85d13b80dffe3d010453ca29 🚀

--- static/js/622.44cdbf17.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkcookbook_app_template=self.webpackChunkcookbook_app_template||[]).push([[622],{4622:(e,o,p)=>{p.r(o),p.d(o,{default:()=>k});var a=p(184);const k=()=>(0,a.jsx)("div",{})}}]);
-//# sourceMappingURL=622.44cdbf17.chunk.js.map
