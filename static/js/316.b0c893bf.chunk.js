54817055679cac4acb3f43a7d06a7c3e828a16e6
lizaklimova <132839549+lizaklimova@users.noreply.github.com>
2024-02-12 18:35:28 +0000

    Deploying to gh-pages from @ lizaklimova/cookbook_app@d5f878027eda4fdc33566aceeccf30dea6f34d3f 🚀

---
+++ static/js/316.b0c893bf.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkcookbook_app_template=self.webpackChunkcookbook_app_template||[]).push([[316],{4316:(t,a,c)=>{c.d(a,{iUH:()=>o});var e=c(2119);function o(t){return(0,e.w_)({tag:"svg",attr:{viewBox:"0 0 448 512"},child:[{tag:"path",attr:{d:"M201.4 342.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 274.7 86.6 137.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z"},child:[]}]})(t)}}}]);
+//# sourceMappingURL=316.b0c893bf.chunk.js.map
